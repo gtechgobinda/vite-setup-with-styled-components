99309dd9e4dcb56b99d95e24b064f31cc8a5eda8
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-11-29 10:04:37 +0530

    fixed all

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AppContext } from "./utils/ContextApi.jsx";
 import TestComponent from "./components/test/index.jsx";
 
 const App = () => {
   return (
-    <AppContext>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={<TestComponent />} />
-        </Routes>
-      </BrowserRouter>
-    </AppContext>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<TestComponent />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
